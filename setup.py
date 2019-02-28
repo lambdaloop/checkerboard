5a074bbcf1e93856cf13fdc2d5a9df29250d90b6
Pierre Karashchuk <krchtchk@gmail.com>
2019-02-27 17:57:14 -0800

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="checkerboard",
-    version="0.1.1",
+    version="0.1.2",
     author="Pierre Karashchuk",
     author_email="krchtchk@gmail.com",
     description="More robust checkerboard detection, similar algorithm to libcbdetect",
